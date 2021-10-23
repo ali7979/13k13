0d4d011903c6da64345e915dfd12831e14962ad4
Zoheb Alam <zohebzob@gmail.com>
2021-10-23 12:05:34 +0530

    ali

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import dshbrd from './screens/dshbrd';
 import hmepg from './screens/hmepg';
 import Signup from './Components/Signup';
 import sigup from './screens/signup';
+import upldsc from './screens/upldsc';
+import signin from './screens/signin';
 
 function App() {
   return ( 
@@ -23,10 +25,13 @@ function App() {
         <Route path="/post" exact component={hmepg} />
         <Route path="/login" exact component={Login} />
         <Route path="/hme" exact component={hmepg} />
-        <Route path="/" exact component={UploadVideoScreen}/>
+        <Route path="/" exact component={upldsc}/>
+        <Route path="/u" exact component={UploadVideoScreen}/>
         <Route path="/dash" exact component={Dashboard}/>
         <Route path="/dsh" exact component={dshbrd}/>
         <Route path="/signup" exact component={sigup}/>
+        <Route path="/signin" exact component={signin}/>
+
         <Route path="/profile/:email" exact component={ProfileScreen}/>
     </Switch>
     </main>
